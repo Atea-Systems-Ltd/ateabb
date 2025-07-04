b4df0ab1bb73ffeb53dcb78eac1531f2f8e4bb6a
Super User <root@localhost.localdomain>
2025-07-04 18:07:09 +1200

    attempt to increase user limit to 50

--- packages/builder/src/stores/portal/licensing.ts
+++ packages/builder/src/stores/portal/licensing.ts
@@ -313,7 +313,8 @@ class LicensingStore extends BudiStore<LicensingState> {
     }
 
     const userQuota = license.quotas.usage.static.users
-    const userLimit = userQuota.value
+//    const userLimit = userQuota.value
+    const userLimit = 50
     const aiCreditsQuota = license.quotas.usage.monthly.budibaseAICredits
     const aiCreditsLimit = aiCreditsQuota.value
     const actionsQuota = license.quotas.usage.monthly.actions
@@ -336,6 +337,7 @@ class LicensingStore extends BudiStore<LicensingState> {
       userLimitExceeded
 
     this.update(state => {
+	    console.log("Update called ***. userLimit = "+userLimit);
       return {
         ...state,
         usageMetrics: { ...monthlyMetrics, ...staticMetrics },
