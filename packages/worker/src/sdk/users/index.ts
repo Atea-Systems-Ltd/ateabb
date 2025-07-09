23e384f69eb58fe7395943b8d0a2ef7fb04f90c5
root <root@localhost.localdomain>
2025-07-10 01:14:31 +1200

    increased user quota on free plan to 500

--- packages/worker/src/sdk/users/index.ts
+++ packages/worker/src/sdk/users/index.ts
@@ -2,6 +2,19 @@ export * from "./users"
 import { users } from "@budibase/backend-core"
 import * as pro from "@budibase/pro"
 // pass in the components which are specific to the worker/the parts of pro which backend-core cannot access
+// 
+//console.log("*********************************************")
+//console.log(
+//  "***** Initialising DB with quotas: \n" + JSON.stringify(pro, (key, value) => {
+//	if(typeof value === 'function') {
+//		return value.toString();
+//	}
+//	return value;	
+//  })
+//)
+console.log("*********************************************")
+// Change the number of users to 500
+pro.constants.licenses.SELF_FREE_LICENSE.quotas.usage.static.users.value = 500;
 users.UserDB.init(pro.quotas, pro.groups, pro.features)
 export const db = users.UserDB
 export { users as core } from "@budibase/backend-core"
