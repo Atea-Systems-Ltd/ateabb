df2ab478262e37177cedf9f92982893528279eb6
callum <callum@ateasystems.com>
2025-09-03 08:20:52 +1200

    moved license "hacks" to initPro.ts

--- packages/worker/src/initPro.ts
+++ packages/worker/src/initPro.ts
@@ -1,5 +1,13 @@
 import { sdk as proSdk } from "@budibase/pro"
 
+import * as pro from "@budibase/pro"
+import { Feature } from "@budibase/types"
+
 export const initPro = async () => {
+  pro.constants.licenses.SELF_FREE_LICENSE.quotas.usage.static.users.value = -1
+  pro.constants.licenses.SELF_FREE_LICENSE.features = [
+    Feature.BRANDING,
+    Feature.OFFLINE,
+  ]
   await proSdk.init({})
 }
--- packages/worker/src/sdk/users/index.ts
+++ packages/worker/src/sdk/users/index.ts
@@ -1,7 +1,6 @@
 export * from "./users"
 import { users } from "@budibase/backend-core"
 import * as pro from "@budibase/pro"
-import { Feature } from "@budibase/types"
 // pass in the components which are specific to the worker/the parts of pro which backend-core cannot access
 //
 //console.log("*********************************************")
@@ -13,22 +12,22 @@ import { Feature } from "@budibase/types"
 //	return value;
 //  })
 //)
-console.log("*********************************************")
-pro.constants.licenses.SELF_FREE_LICENSE.quotas.usage.static.users.value = -1
-//pro.constants.licenses.SELF_FREE_LICENSE.quotas.usage.static.userGroups.value = -1
-// Feature enum defined in ./packages/types/src/sdk/licensing/feature.ts
-//pro.constants.licenses.SELF_FREE_LICENSE.features = [ "environmentVariables", "userGroups", "branding","pwa","appBackups", "offline", "pdf" ]
-//pro.constants.licenses.SELF_FREE_LICENSE.features = [ "branding", "environmentVariables", "offline" ]
-pro.constants.licenses.SELF_FREE_LICENSE.features = [
-  Feature.BRANDING,
-  // Feature.ENVIRONMENT_VARIABLES,
-  Feature.OFFLINE,
-  // Feature.SYNC_AUTOMATIONS,
-  // Feature.TRIGGER_AUTOMATION_RUN,
-  // Feature.USER_GROUPS,
-  // Feature.PWA,
-  // Feature.PDF,
-]
+// console.log("*********************************************")
+// pro.constants.licenses.SELF_FREE_LICENSE.quotas.usage.static.users.value = -1
+// //pro.constants.licenses.SELF_FREE_LICENSE.quotas.usage.static.userGroups.value = -1
+// // Feature enum defined in ./packages/types/src/sdk/licensing/feature.ts
+// //pro.constants.licenses.SELF_FREE_LICENSE.features = [ "environmentVariables", "userGroups", "branding","pwa","appBackups", "offline", "pdf" ]
+// //pro.constants.licenses.SELF_FREE_LICENSE.features = [ "branding", "environmentVariables", "offline" ]
+// pro.constants.licenses.SELF_FREE_LICENSE.features = [
+//   Feature.BRANDING,
+//   // Feature.ENVIRONMENT_VARIABLES,
+//   Feature.OFFLINE,
+//   // Feature.SYNC_AUTOMATIONS,
+//   // Feature.TRIGGER_AUTOMATION_RUN,
+//   // Feature.USER_GROUPS,
+//   // Feature.PWA,
+//   // Feature.PDF,
+// ]
 // PlanType enum defined in  ./packages/types/src/sdk/licensing/plan.ts
 // pro.constants.licenses.SELF_FREE_LICENSE.plan.type = PlanType.ENTERPRISE
 users.UserDB.init(pro.quotas, pro.groups, pro.features)
